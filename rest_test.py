00244a4799ba3df22e20ab47c31a678df81719b7
007lva <007.lva@gmail.com>
2015-02-14 02:25:49 +0000

    python3 support

--- rest_test.py
+++ rest_test.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-from webtest import TestApp, Upload
-from webtest.compat import to_bytes
+from webtest import TestApp, compat
 import rest, unittest
 
 class TestRestService(unittest.TestCase):
 
     def setUp(self):
         self.app = TestApp(rest.app)
-    
+
     def test_upload(self):
-        self.file_name = '007lva.jpg'
-        self.image = file('./source_test_images/%s' % self.file_name)
-        self.uploaded_file_contents = self.image.read()
-        self.resp = self.app.post('/Images',
-                                  upload_files=[('file',
-                                                 self.file_name,
-                                                 self.uploaded_file_contents)])
-        assert self.resp.status == '200 OK'
-        
+        file_name = '007lva.jpg'
+        with open('./source_test_images/%s' % file_name, 'rb')\
+            as image:
+            content = compat.to_bytes(image.read())
+            resp = self.app.post('/Images',
+                                  upload_files=[('file', file_name, content)])
+            assert resp.status == '200 OK'
+    """
     def test_retrieve_image(self):
         self.file_name = '007lva.jpg'
         self.image = file('./source_test_images/%s' % self.file_name)
@@ -28,7 +25,7 @@ class TestRestService(unittest.TestCase):
                                                  self.uploaded_file_contents)])
         self.resp = self.app.get('/Images/1')
         assert self.resp.status == '200 OK'
-        
+
     def test_retrieve_nonexistent_image(self):
         self.file_name = '007lva.jpg'
         self.image = file('./source_test_images/%s' % self.file_name)
@@ -38,15 +35,15 @@ class TestRestService(unittest.TestCase):
         self.resp = self.app.get('/Images/5', expect_errors = True, 
                                  status = 500)
         assert self.resp.status == '500 Internal Server Error'
-        
+
     def test_prueba(self):
         self.resp = self.app.post('/test')
         assert self.resp.status == '200 OK'
-    
+
     def test_error404(self):
         self.resp = self.app.get('/Dummy', expect_errors=True, status = 404)
         assert self.resp.status == '404 Not Found'
-        
+
     def test_error400_with_badfile(self):
         self.file_name = 'bad_file_test.txt'
         self.image = file('./source_test_images/%s' % self.file_name)
@@ -61,3 +58,4 @@ class TestRestService(unittest.TestCase):
 
     def tearDown(self):
         pass
+    """
